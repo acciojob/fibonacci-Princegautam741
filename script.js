503f3d4817732cb7865c6a4b94e9df093b8044be
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-11 17:49:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
-// your code here
+    if (num <= 0) {
+        return "Invalid input. Please provide a positive integer greater than 0.";
+    } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        let a = 0, b = 1, temp;
+        for (let i = 3; i <= num; i++) {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
 }
 
 module.exports = fibonacci;
